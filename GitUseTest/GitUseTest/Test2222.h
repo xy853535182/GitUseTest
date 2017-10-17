005ccd7fe5b4b96de1e2a2463d78fafe960dfd1e
125520@nd.com <494326923@qq.com>
2017-10-17 16:09:30 +0800

    邮件相关

---
+++ GitUseTest/GitUseTest/Test2222.h
@@ -0,0 +1,15 @@
+//
+//  Test2222.h
+//  GitUseTest
+//
+//  Created by nd on 17/10/17.
+//  Copyright © 2017年 nd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test2222 : NSObject
+
+- (void)log111;
+
+@end
